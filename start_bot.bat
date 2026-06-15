cfda9b1575685974275848617c262010facf7c6a
Chemitha Sathsliu <137410563+chemitha@users.noreply.github.com>
2026-06-15 16:20:35 +0530

    Fix script to start correct Python file

--- start_bot.bat
+++ start_bot.bat
@@ -4,6 +4,6 @@ title start_bot
 ::Attention! This code must be debug because this code kills all the python.exe
 
 taskkill /im python.exe /f
-start /b python neko_seeker_main.py
+start /b python _seeker_main.py
 timeout /t 1200
 goto loop
