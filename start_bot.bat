dd77a0421fd239e48f7bc7f4806363ec948a7f61
Chemitha Sathsliu <137410563+chemitha@users.noreply.github.com>
2026-06-15 16:20:23 +0530

    Update bot script to use relative path for Python script

--- start_bot.bat
+++ start_bot.bat
@@ -4,6 +4,6 @@ title start_bot
 ::Attention! This code must be debug because this code kills all the python.exe
 
 taskkill /im python.exe /f
-start /b python C:\Users\DELL\Documents\TelegramBot\neko_seeker_bot\neko_seeker_main.py
+start /b python neko_seeker_main.py
 timeout /t 1200
 goto loop
